af0dba8db78da8139aaa93b9eb28e86555985c79
Michael Snoyman <michael@snoyman.com>
2017-07-12 10:51:31 +0300

    Implemented Exception module

--- unliftio/src/UnliftIO.hs
+++ unliftio/src/UnliftIO.hs
@@ -3,9 +3,12 @@ module UnliftIO
   , module UnliftIO.Async
   , module UnliftIO.Chan
   , module UnliftIO.Exception
+  , module UnliftIO.IO
   , module UnliftIO.IORef
   , module UnliftIO.Instances
   , module UnliftIO.MVar
+  , module UnliftIO.Resource
+  , module UnliftIO.Temporary
   , module UnliftIO.Timeout
   ) where
 
@@ -13,7 +16,10 @@ import Control.Monad.IO.Unlift
 import UnliftIO.Async
 import UnliftIO.Chan
 import UnliftIO.Exception
+import UnliftIO.IO
 import UnliftIO.IORef
 import UnliftIO.Instances
 import UnliftIO.MVar
+import UnliftIO.Resource
+import UnliftIO.Temporary
 import UnliftIO.Timeout
